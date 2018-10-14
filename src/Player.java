708ccd597445c2acb64eabed8796b0042e8da69f
JB <jbladevr@yahoo.com>
2018-10-14 12:12:09 -0600

    added comments from interface and some updates

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,34 @@
+/**
+ * A player of the Spin-the-Wheel Coin Matching Game.
+ *
+ * @author CS4250 Fall 2018
+ * @version 1.2.2 (20181004)
+ */
 public class Player implements StrategicPlayer {
     private int coinsPerWheel;
     private int revealsPerSpin;
     private int maxNumSpins;
 
+    /**
+     * Establishes that the player is beginning a new game.
+     * @param coinsPerWheel the number of coins in the wheel
+     * @param revealsPerSpin the number of coins revealed per turn/spin
+     * @param maxNumSpins the maximum number of spin allowed for the game
+     */
     public void beginGame(int coinsPerWheel, int revealsPerSpin, int maxNumSpins){
         this.coinsPerWheel = coinsPerWheel;
         this.revealsPerSpin = revealsPerSpin;
         this.maxNumSpins = maxNumSpins;
     }
 
+    /**
+     * Provides the request pattern for the current turn.
+     * The returned pattern is of proper length, contains only
+     * '?' and '-', and has exactly the number of '?'
+     * as the permitted reveals per spin.
+     * @return a proper reveal-pattern consisting of '-' and '?' with
+     *         exactly the number of '?' as permitted by reveals-per-spin
+     */
     public CharSequence getSlotsToReveal(){
         int count = revealsPerSpin;
         StringBuilder stringBuilder = new StringBuilder();
@@ -23,6 +43,15 @@ public class Player implements StrategicPlayer {
     return stringBuilder.toString();
     }
 
+    /**
+     * Provides the coin-state set pattern for the current turn.
+     * The returned pattern is a copy of the parameter
+     * in which all '-' are preserved and each location of
+     * 'H' and 'T' may be replaced by either 'H' or 'T'.
+     * @param revealedPattern a proper-length pattern
+     *                        consisting of '-', 'H', and 'T'
+     * @return a proper set-pattern consisting of '-', 'H', and 'T'
+     */
     public CharSequence getNewCoinStates(CharSequence revealedPattern){
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(revealedPattern);
@@ -30,8 +59,7 @@ public class Player implements StrategicPlayer {
             if (stringBuilder.charAt(i) == '?' )
                 stringBuilder.replace(i,(i+1),"H");
         }
-
-        return stringBuilder.toString();
+    return stringBuilder.toString();
     }
 
 }
--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -1,6 +1,8 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 public class PlayerTest {
     @Before
@@ -11,9 +13,18 @@ public class PlayerTest {
     public void tearDown() {
     }
 
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
+
     @Test
     public void beginGameTest(){
 
+    }
+    @Test
+    public void exceptionGameTestArgsInvalid(){
+        thrown.expect(IllegalArgumentException.class);
+        Player player = new Player();
+        player.beginGame(-1,-1,-1);
     }
 
     @Test
@@ -23,7 +34,7 @@ public class PlayerTest {
 
     @Test
     public void getNewCoinStatesTest(){
-
+        
     }
 
 
