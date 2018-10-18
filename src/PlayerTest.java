dcb0ecf00142288bfd388847cd3f7e647df4c6f3
Jbladevr <jbladevrmain@yahoo.com>
2018-10-17 23:18:53 -0600

    Update PlayerTest.java
    fixed some checkstyle problems

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -2,7 +2,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assert;
 
 public class PlayerTest {
     @Before
@@ -14,53 +14,53 @@ public class PlayerTest {
     }
 
     @Test
-    public void beginGameNotNullTest(){
+    public void beginGameNotNullTest() {
         Player player = new Player();
         assertNotNull(player);
     }
 
     @Test
-    public void getSlotsToRevealTest(){
+    public void getSlotsToRevealTest() {
         Player player = new Player();
-        player.beginGame(10,4,5);
-        assertEquals(player.getSlotsToReveal(),"????------");
+        player.beginGame(10, 4, 5);
+        assertEquals(player.getSlotsToReveal(), "????------");
     }
 
     @Test
-    public void getNewCoinStatesTest(){
+    public void getNewCoinStatesTest() {
         Player player = new Player();
-        player.beginGame(6,3,10);
-        assertEquals(player.getNewCoinStates("TTH---"),"HHH---");
+        player.beginGame(6, 3, 10);
+        assertEquals(player.getNewCoinStates("TTH---"), "HHH---");
     }
     @Test
-    public void getNewCoinStatesFourTwoFirstTest(){
+    public void getNewCoinStatesFourTwoFirstTest() {
         Player player = new Player();
-        player.beginGame(4,2,10);
-        assertEquals(player.getNewCoinStates("HT--"),"HH--");
+        player.beginGame(4, 2, 10);
+        assertEquals(player.getNewCoinStates("HT--"), "HH--");
     }
     @Test
-    public void getNewCoinStatesFourTwoSecondTest(){
+    public void getNewCoinStatesFourTwoSecondTest() {
         Player player = new Player();
-        player.beginGame(4,2,10);
-        assertEquals(player.getNewCoinStates("HH--"),"TT--");
+        player.beginGame(4, 2, 10);
+        assertEquals(player.getNewCoinStates("HH--"), "TT--");
     }
     @Test
-    public void getNewCoinStatesFourTwoThirdTest(){
+    public void getNewCoinStatesFourTwoThirdTest() {
         Player player = new Player();
-        player.beginGame(4,2,10);
-        assertEquals(player.getNewCoinStates("TT--"),"HH--");
+        player.beginGame(4, 2, 10);
+        assertEquals(player.getNewCoinStates("TT--"), "HH--");
     }
     @Test
-    public void getSlotsToRevealFourTwoSecondTest(){
+    public void getSlotsToRevealFourTwoSecondTest() {
         Player player = new Player();
-        player.beginGame(4,2,5);
+        player.beginGame(4, 2, 5);
         player.getSlotsToReveal();
-        assertEquals(player.getSlotsToReveal(),"?-?-");
+        assertEquals(player.getSlotsToReveal(), "?-?-");
     }
     @Test
-    public void getSlotsToRevealFourTwoFirstTest(){
+    public void getSlotsToRevealFourTwoFirstTest() {
         Player player = new Player();
-        player.beginGame(4,2,5);
-        assertEquals(player.getSlotsToReveal(),"??--");
+        player.beginGame(4, 2, 5);
+        assertEquals(player.getSlotsToReveal(), "??--");
     }
 }
